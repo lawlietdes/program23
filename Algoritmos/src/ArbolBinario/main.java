f5ce7ee0df920584c533f89d1dbf61e2b0b4671c
Luis Antonio Flores Arriaga <luisprogram23@gmail.com>
2020-01-27 12:00:33 -0600

    Bitch comment

--- Algoritmos/src/ArbolBinario/main.java
+++ Algoritmos/src/ArbolBinario/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Arbol arbol = new Arbol();
-        
+        // putita
         arbol.insertar (9,"Manzanas");
         arbol.insertar(3,"Peras");
         arbol.insertar(5,"Uvas");
